f71917ac6cd7ba7c6079a875d541db89461964f1
Serhii Soboliev <sergiy.sobolev@gmail.com>
2022-01-10 20:46:36 +0200

    [310. Minimum Height Trees] find all paths from vertex

--- src/main/scala/MinimalHeightTree.scala
+++ src/main/scala/MinimalHeightTree.scala
@@ -9,12 +9,37 @@ https://leetcode.com/problems/minimum-height-trees/
 class MinimalHeightTree {
     def findMinHeightTrees(n: Int, edges: Array[Array[Int]]): List[Int] = {
         val edgesMap = buildEdgesMap(n, edges)
+        findDiameter(edgesMap)
         null
     }
 
-    def findDiameter(n: Int, edges: Array[Array[Int]]) : Int = {
+    def findDiameter(edgesMap: Array[ArrayBuffer[Int]]) : Int = {
+        val firstVertex = 0
+        val paths = ArrayBuffer(ArrayBuffer(firstVertex))
+        val allPaths = findAllPathsFromVertex(paths, edgesMap)
         0
     }
+
+    def findAllPathsFromVertex(paths: ArrayBuffer[ArrayBuffer[Int]], edgesMap: Array[ArrayBuffer[Int]]) : ArrayBuffer[ArrayBuffer[Int]] ={
+        val newPaths:ArrayBuffer[ArrayBuffer[Int]] = ArrayBuffer()
+        for(path <- paths) {
+            val accessibleVertexes = edgesMap(path.last)
+            for (av <- accessibleVertexes) {
+                if (!path.contains(av)) {
+                    val newPath = path.clone()
+                    newPath += av
+                    newPaths += newPath
+                }
+            }
+        }
+        if(newPaths.nonEmpty) {
+            paths.clear()
+            paths.addAll(newPaths)
+            findAllPathsFromVertex(paths, edgesMap)
+        }
+        paths
+    }
+
 
     private def buildEdgesMap(n: Int, edges: Array[Array[Int]]): Array[ArrayBuffer[Int]] = {
         val r = Array.fill[ArrayBuffer[Int]](n)(ArrayBuffer.empty[Int])
@@ -31,6 +56,7 @@ class MinimalHeightTree {
 object MinimalHeightTree {
     def main(args: Array[String]): Unit = {
         val o = new MinimalHeightTree
-        o.findMinHeightTrees(5, Array(Array(1, 0), Array(2, 0), Array(3, 1), Array(3, 2), Array(2,1)))
+       // o.findMinHeightTrees(4, Array(Array(1, 0), Array(2, 0), Array(3, 1), Array(3, 2), Array(2,1)))
+        o.findMinHeightTrees(4, Array(Array(1, 0), Array(1, 2), Array(3, 1)))
     }
 }
