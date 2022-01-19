ab0a8cb74245cfd2b4d3ffc9f65426ba83f5e04a
Serhii Soboliev <sergiy.sobolev@gmail.com>
2022-01-19 03:42:08 +0200

    [1462. Course Schedule IV] DFS solution with memoization

--- src/main/scala/CourseSchedule4.scala
+++ src/main/scala/CourseSchedule4.scala
@@ -12,21 +12,25 @@ class CourseSchedule4 {
   def checkIfPrerequisite(numCourses: Int, prerequisites: Array[Array[Int]], queries: Array[Array[Int]]): List[Boolean] = {
     val n = numCourses
     val ans = new ListBuffer[Boolean]()
-    val map: mutable.Map[Int, ArrayBuffer[Int]] = mutable.Map[Int, ArrayBuffer[Int]]()
+    val map = Array.fill[mutable.Set[Int]](n)(mutable.Set[Int]())
 
-    def buildGraph(): Array[mutable.Set[Int]] = {
-      val g = Array.fill[mutable.Set[Int]](n)(mutable.Set.empty[Int])
+    def buildGraph(): Array[ArrayBuffer[Int]] = {
+      val g = Array.fill[ArrayBuffer[Int]](n)(ArrayBuffer[Int]())
       prerequisites foreach(p => g(p(0)) += p(1))
       g
     }
 
     val graph = buildGraph()
 
-    def dfs(i: Int, visited: Array[Boolean], path: ArrayBuffer[Int]) : ArrayBuffer[Int] = {
+    def dfs(i: Int, visited: Array[Boolean], path: mutable.Set[Int]) : mutable.Set[Int] = {
       visited(i) = true
       for(c <- graph(i)) {
         if(!visited(c)) {
-          dfs(c, visited, path)
+          if (map(c).nonEmpty) {
+            path.addAll(map(c))
+          } else {
+            dfs(c, visited, path)
+          }
         }
       }
       path += i
@@ -34,8 +38,10 @@ class CourseSchedule4 {
     }
 
     def buildMap() : Unit = {
+
       for(i <- 0 until numCourses) {
-        map(i) = dfs(i, Array.fill[Boolean](numCourses)(false), ArrayBuffer[Int]())
+        val res = dfs(i, Array.fill[Boolean](numCourses)(false), mutable.Set[Int]())
+        map(i) = res
       }
     }
 
