8df15794dc70034fe59bd3e9581e31b7582ecce3
Serhii Soboliev <sergiy.sobolev@gmail.com>
2022-01-23 04:22:55 +0200

    [67. Add Binary] Recursive solution

--- src/main/scala/bitmanipulation/AddBinary.scala
+++ src/main/scala/bitmanipulation/AddBinary.scala
@@ -7,7 +7,35 @@ https://leetcode.com/problems/add-binary/
  */
 class AddBinary {
 
-    def addBinary(as: String, bs: String): String = {
+    def addBinary(a: String, b: String): String = {
+
+        def addChar(c1: Char, c2: Char,c3: Char): String = {
+            (c1,c2,c3) match {
+                case ('1','1','1') => "11"
+                case ('0','0','0') => "00"
+                case ('1','0','1') => "10"
+                case ('1','1','0') => "10"
+                case ('0','1','1') => "10"
+                case _ => "01"
+            }
+        }
+
+        @annotation.tailrec
+        def add(l1: List[Char], l2: List[Char], s: String): String = {
+            (l1,l2) match {
+                case (Nil,Nil) => s
+                case (h1::t1,Nil) => add(t1,l2,addChar(h1,s(0),'0') + s.tail)
+                case (Nil,h2::t2) => add(l1,t2,addChar(h2,s(0),'0') + s.tail)
+                case (h1::t1,h2::t2) => add(t1,t2,addChar(h1,h2,s(0)) + s.tail)
+            }
+        }
+
+        val s = add(a.reverse.toList,b.reverse.toList,"0").dropWhile(_=='0')
+        if (s.isEmpty) "0"
+        else s
+    }
+
+    def addBinaryIterative(as: String, bs: String): String = {
         var res = ""
         val d = as.length - bs.length
         var an = as
--- src/test/scala/bitmanipulation/AddBinarySpec.scala
+++ src/test/scala/bitmanipulation/AddBinarySpec.scala
@@ -4,18 +4,30 @@ package bitmanipulation
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class AddBinarySpec extends AnyFlatSpec with Matchers{
+class AddBinarySpec extends AnyFlatSpec with Matchers {
 
-    "AddBinary('11', '1')" should "100" in {
-        new AddBinary().addBinary("11", "1") shouldEqual "100"
-    }
-
-    "AddBinary('1', '1')" should "10" in {
-        new AddBinary().addBinary("1", "1") shouldEqual "10"
+    "AddBinary('1111', '110001')" should "1000000" in {
+        new AddBinary().addBinary("1111", "110001") shouldEqual "1000000"
     }
 
     "AddBinary('1111', '1111')" should "11110" in {
         new AddBinary().addBinary("1111", "1111") shouldEqual "11110"
+    }
+
+    "AddBinaryIterative('11', '1')" should "100" in {
+        new AddBinary().addBinaryIterative("11", "1") shouldEqual "100"
+    }
+
+    "AddBinaryIterative('1', '1')" should "10" in {
+        new AddBinary().addBinaryIterative("1", "1") shouldEqual "10"
+    }
+
+    "AddBinaryIterative('1111', '1111')" should "11110" in {
+        new AddBinary().addBinaryIterative("1111", "1111") shouldEqual "11110"
+    }
+
+    "AddBinaryIterative('1111', '110001')" should "1000000" in {
+        new AddBinary().addBinaryIterative("1111", "110001") shouldEqual "1000000"
     }
 
 }
