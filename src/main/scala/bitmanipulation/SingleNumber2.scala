9d5ebbaa68425f91670a37944ba5b59b406fa15a
Serhii Soboliev <sergiy.sobolev@gmail.com>
2022-01-26 03:07:16 +0200

    [137. Single Number II] Bit counting

--- src/main/scala/bitmanipulation/SingleNumber2.scala
+++ src/main/scala/bitmanipulation/SingleNumber2.scala
@@ -1,13 +1,25 @@
 package org.sbk.leet
 package bitmanipulation
 
+import scala.collection.mutable.ArrayBuffer
+
 /*
 137. Single Number II
 https://leetcode.com/problems/single-number-ii/
 https://baihuqian.github.io/2018-06-23-single-number-ii/
  */
 class SingleNumber2 {
+
     def singleNumber(nums: Array[Int]): Int = {
+        (0 until 32)
+          .map(i => nums.map(n => (n >> i) & 1).sum % 3)
+          .toArray
+          .zipWithIndex
+          .map(v => v._1 * Math.pow(2, v._2).toInt)
+          .sum
+    }
+
+    def singleNumberKarnaughMap(nums: Array[Int]): Int = {
         var ones = 0
         var twos = 0
         for (n <- nums) {
--- src/test/scala/bitmanipulation/SingleNumber2Spec.scala
+++ src/test/scala/bitmanipulation/SingleNumber2Spec.scala
@@ -6,12 +6,24 @@ import org.scalatest.matchers.should.Matchers
 
 class SingleNumber2Spec extends AnyFlatSpec with Matchers{
 
+    ignore should "-4" in {
+        new SingleNumber2().singleNumber(Array(-2,-2,1,1,4,1,4,4,-4,-2)) should equal (-4)
+    }
+
     "SingleNumber2.singleNumber(Array(2,2,3,2))" should "3" in {
         new SingleNumber2().singleNumber(Array(2,2,3,2)) should equal (3)
     }
 
     "SingleNumber2.singleNumber(Array(0,1,0,1,0,1,99))" should "99" in {
         new SingleNumber2().singleNumber(Array(0,1,0,1,0,1,99)) should equal (99)
+    }
+
+    "SingleNumber2.singleNumberKarnaughMap(Array(2,2,3,2))" should "3" in {
+        new SingleNumber2().singleNumberKarnaughMap(Array(2,2,3,2)) should equal (3)
+    }
+
+    "SingleNumber2.singleNumberKarnaughMap(Array(0,1,0,1,0,1,99))" should "99" in {
+        new SingleNumber2().singleNumberKarnaughMap(Array(0,1,0,1,0,1,99)) should equal (99)
     }
 
 }
