13902d2b003140149417004da366442f22151653
Serhii Soboliev <sergiy.sobolev@gmail.com>
2022-01-26 17:28:32 +0200

    [980. Unique Paths III] DFS restart

--- src/main/scala/bitmanipulation/UniquePaths3.scala
+++ src/main/scala/bitmanipulation/UniquePaths3.scala
@@ -9,68 +9,42 @@ class UniquePaths3 {
 
     def uniquePathsIII(grid: Array[Array[Int]]): Int = {
         import scala.collection.mutable.ArrayBuffer
-        import scala.util.control.Breaks._
 
         val n = grid.length
         val m = grid.headOption.get.length
 
-        def findStartingPoint() : (Int, Int) = {
-            for{
-                i <- 0 until n
-                j <- 0 until m
-            } {
-                if(grid(i)(j) == 1) {
-                    return (i, j)
-                }
-            }
-            (-1,-1)
+        def findStartingPoint(): IndexedSeq[(Int, Int)] = {
+            for {
+                i <- grid.indices
+                j <- grid(i).indices
+                if grid(i)(j) == 1
+            } yield (i, j)
         }
 
         def findNeighbors(i: Int, j: Int): Array[(Int, Int)] = {
             val nei = ArrayBuffer[(Int, Int)]()
-            if (i > 0) {
-                nei += Tuple2(i - 1, j)
-            }
-            if (i < n - 1) {
-                nei += Tuple2(i + 1, j)
-            }
-            if (j > 0) {
-                nei += Tuple2(i, j - 1)
-            }
-            if (j < m - 1) {
-                nei += Tuple2(i, j + 1)
-            }
+            if (i > 0) nei += Tuple2(i - 1, j)
+            if (i < n - 1) nei += Tuple2(i + 1, j)
+            if (j > 0) nei += Tuple2(i, j - 1)
+            if (j < m - 1) nei += Tuple2(i, j + 1)
             nei.toArray
         }
 
         var pathsCount = 0
-        val zeroCount = grid.flatten.count(n => n == 0) + 1
-        def dfs(i: Int, j: Int, visited: ArrayBuffer[(Int, Int)]): Unit = {
-            visited += Tuple2(i,j)
-            for((k,t) <- findNeighbors(i, j)) {
-                breakable {
-                    if(visited.contains((k,t))) {
-                        break
-                    }
-                    val v = grid(k)(t)
-                    if(v == -1) {
-                        break
-                    }
-                    if (v == 2) {
-                        if (visited.length == zeroCount) {
-                            pathsCount += 1
-                        }
-                    }
-                    if (v == 0 && visited.length < zeroCount) {
-                        dfs(k, t, visited.clone())
-                    }
-                }
-            }
-        }
-        val (i, j) = findStartingPoint()
-        dfs(i, j, ArrayBuffer())
-        pathsCount
+        val pathLength = grid.flatten.count(n => n == 0) + 1
 
+        def dfs(i: Int, j: Int, visited: ArrayBuffer[(Int, Int)]): Unit = {
+            visited += Tuple2(i, j)
+            findNeighbors(i, j)
+              .filter(l => !visited.contains(l))
+              .filter(l => grid(l._1)(l._2) != 1)
+              .foreach { case(k, t) =>
+                  if (grid(k)(t) == 2 && visited.length == pathLength) pathsCount += 1
+                  if (grid(k)(t) == 0 && visited.length < pathLength) dfs(k, t, visited.clone())
+              }
+        }
+        findStartingPoint().headOption.foreach(n => dfs(n._1, n._2, ArrayBuffer()))
+        pathsCount
     }
 
 }
