98c5eeb04e177a227a68e9e202ccd2de1f58b448
Serhii Soboliev <sergiy.sobolev@gmail.com>
2022-02-03 01:12:18 +0200

    [187. Repeated DNA Sequences] Sliding window solution solution

--- src/main/scala/hashtables/RepeatedDNASequences.scala
+++ src/main/scala/hashtables/RepeatedDNASequences.scala
@@ -10,12 +10,24 @@ https://leetcode.com/problems/repeated-dna-sequences/
 class RepeatedDNASequences {
 
     def findRepeatedDnaSequences(s: String): List[String] = {
+        s.sliding(10)
+          .toList
+          .groupBy(identity)
+          .view
+          .mapValues(_.size)
+          .filter(_._2 > 1)
+          .keys
+          .toList
+    }
+
+
+    def findRepeatedDnaSequencesNaive(s: String): List[String] = {
         import scala.collection.mutable
         if(s.length < 11) return List[String]()
         val seqCnt: mutable.Map[String, Int] = new mutable.HashMap[String, Int]().withDefaultValue(0)
         for(i <- 0 to s.length - 10) {
             val sub = s.slice(i, i+10)
-            seqCnt.update(sub, seqCnt(sub) + 1)
+            seqCnt(sub) = seqCnt.getOrElse(sub, 0) + 1
         }
         seqCnt
           .filter{ case(_, v) => v > 1}
--- src/test/scala/hashtables/RepeatedDNASequencesSpec.scala
+++ src/test/scala/hashtables/RepeatedDNASequencesSpec.scala
@@ -5,9 +5,7 @@ package hashtables
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-
 class RepeatedDNASequencesSpec extends AnyFlatSpec with Matchers{
-
 
     "RepeatedDNASequences.findRepeatedDnaSequences('AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT')" should "be ['AAAAACCCCC','CCCCCAAAAA']" in {
         new RepeatedDNASequences()
@@ -16,11 +14,25 @@ class RepeatedDNASequencesSpec extends AnyFlatSpec with Matchers{
 
     "RepeatedDNASequences.findRepeatedDnaSequences('AAAAAAAAAAAAA')" should "be ['AAAAAAAAAAAAA']" in {
         new RepeatedDNASequences()
-          .findRepeatedDnaSequences("AAAAAAAAAAAAA") should contain ("AAAAAAAAAAAAA")
+          .findRepeatedDnaSequences("AAAAAAAAAAAAA") should contain ("AAAAAAAAAA")
     }
 
     "RepeatedDNASequences.findRepeatedDnaSequences('AAAAAAAAAAA')" should "be ['AAAAAAAAAA']" in {
         new RepeatedDNASequences().findRepeatedDnaSequences("AAAAAAAAAAA") should contain ("AAAAAAAAAA")
+    }
+
+    "RepeatedDNASequences.findRepeatedDnaSequencesNaive('AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT')" should "be ['AAAAACCCCC','CCCCCAAAAA']" in {
+        new RepeatedDNASequences()
+          .findRepeatedDnaSequencesNaive("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT") should contain allOf("AAAAACCCCC", "CCCCCAAAAA")
+    }
+
+    "RepeatedDNASequences.findRepeatedDnaSequencesNaive('AAAAAAAAAAAAA')" should "be ['AAAAAAAAAAAAA']" in {
+        new RepeatedDNASequences()
+          .findRepeatedDnaSequencesNaive("AAAAAAAAAAAAA") should contain ("AAAAAAAAAA")
+    }
+
+    "RepeatedDNASequences.findRepeatedDnaSequencesNaive('AAAAAAAAAAA')" should "be ['AAAAAAAAAA']" in {
+        new RepeatedDNASequences().findRepeatedDnaSequencesNaive("AAAAAAAAAAA") should contain ("AAAAAAAAAA")
     }
 
 }
