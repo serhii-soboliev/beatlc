906f4b22936cd65c5ff6dc92db9d0d6e6d386465
Serhii Soboliev <sergiy.sobolev@gmail.com>
2022-02-13 16:50:10 +0200

    [3. Longest Substring Without Repeating Characters] Optimized solution

--- src/main/scala/slidingwindow/LongestSubstringWithoutRepeatingCharacters.scala
+++ src/main/scala/slidingwindow/LongestSubstringWithoutRepeatingCharacters.scala
@@ -1,11 +1,31 @@
 package org.sbk.leet
 package slidingwindow
 
+import scala.collection.mutable
+
 /*
 3. Longest Substring Without Repeating Characters
 https://leetcode.com/problems/longest-substring-without-repeating-characters/
  */
 class LongestSubstringWithoutRepeatingCharacters {
+
+    def lengthOfLongestSubstringOptimized(s: String): Int = {
+        import scala.collection.mutable
+        var lsl = 0
+        val map = new mutable.HashMap[Char, Int]()
+        var i = 0
+        (0 until s.length) foreach(
+          j=> {
+                if (map.contains(s(j))) {
+                    i = Math.max(map.getOrElse(s(j), 0), i)
+                }
+                lsl = Math.max(lsl, j - i + 1)
+                map.put(s(j), j + 1)
+            }
+          )
+        lsl
+    }
+
 
     def lengthOfLongestSubstringNaive(s: String): Int = {
         import scala.collection.mutable
@@ -13,13 +33,13 @@ class LongestSubstringWithoutRepeatingCharacters {
         var lsl = 0
         var startIdx = 0
         while (startIdx < s.length) {
-            val lastSymbolIdx: mutable.Map[Char, Int] = new mutable.HashMap[Char, Int]()
+            val lastSymbolIdx = new mutable.HashMap[Char, Int]()
             breakable {
                 (startIdx until s.length) foreach (
                   i => {
-                      val c: Char = s(i)
+                      val c = s(i)
                       if (lastSymbolIdx.contains(c)) {
-                          val lastIndex: Int = lastSymbolIdx.getOrElse(c, 0)
+                          val lastIndex = lastSymbolIdx.getOrElse(c, 0)
                           lsl = Math.max(lsl, i - startIdx)
                           startIdx = lastIndex + 1
                           break
