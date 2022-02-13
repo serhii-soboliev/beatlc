72bfd06a0cf5fa9337b8e63279b77c7787502aff
Serhii Soboliev <sergiy.sobolev@gmail.com>
2022-02-13 18:08:28 +0200

    [3. Longest Substring Without Repeating Characters] Scala like solution

--- src/main/scala/slidingwindow/LongestSubstringWithoutRepeatingCharacters.scala
+++ src/main/scala/slidingwindow/LongestSubstringWithoutRepeatingCharacters.scala
@@ -9,20 +9,51 @@ https://leetcode.com/problems/longest-substring-without-repeating-characters/
  */
 class LongestSubstringWithoutRepeatingCharacters {
 
+    def lengthOfLongestSubstring(s: String): Int = {
+        val indices = mutable.Map[Char, Int]()
+        var left = 0
+        var longest = 0
+        for ((char, right) <- s.zipWithIndex) {
+            indices get char match {
+                case None =>
+                case Some(i) => left = left max (i + 1)
+            }
+            longest = longest max (right - left + 1)
+            indices.put(char, right)
+        }
+        longest
+    }
+
+    def lengthOfLongestSubstringScala(s: String): Int = {
+
+        val visited = scala.collection.mutable.Map[Char, Integer]()
+
+        val (ans, _) = s.indices.foldLeft(0, 0) {
+            case ((result, begin), idx) if !visited.contains(s(idx)) =>
+                visited += (s(idx) -> idx)
+                (Math.max(result, idx + 1 - begin), begin)
+
+            case ((result, begin), idx) =>
+                // Since we are doing a Map, it is likely a reoccurence can occur for a character
+                // prior to the current begin index. Hence, we take a max of indexes.
+                val old = Math.max(visited(s(idx)) + 1, begin)
+                visited += (s(idx) -> idx)
+                (Math.max(result, idx - old + 1), old)
+        }
+
+        ans
+    }
+
     def lengthOfLongestSubstringOptimized(s: String): Int = {
         import scala.collection.mutable
         var lsl = 0
         val map = new mutable.HashMap[Char, Int]()
         var i = 0
-        (0 until s.length) foreach(
-          j=> {
-                if (map.contains(s(j))) {
-                    i = Math.max(map.getOrElse(s(j), 0), i)
-                }
-                lsl = Math.max(lsl, j - i + 1)
-                map.put(s(j), j + 1)
-            }
-          )
+        for ((c, j) <- s.zipWithIndex) {
+            if (map.contains(c)) i = map.getOrElse(c, 0) max i
+            lsl = lsl max j - i + 1
+            map(c) =  j + 1
+        }
         lsl
     }
 
--- src/test/scala/slidingwindow/LongestSubstringWithoutRepeatingCharactersSpec.scala
+++ src/test/scala/slidingwindow/LongestSubstringWithoutRepeatingCharactersSpec.scala
@@ -7,36 +7,43 @@ import org.scalatest.matchers.should.Matchers
 class LongestSubstringWithoutRepeatingCharactersSpec extends AnyFlatSpec with Matchers{
 
     "Longest Substring Without Repeating Characters of cdd" should "2" in {
+        new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstringScala("cdd") should equal (2)
         new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstringOptimized("cdd") should equal (2)
         new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstringNaive("cdd") should equal (2)
     }
 
     "Longest Substring Without Repeating Characters of tmmzuxt" should "5" in {
+        new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstringScala("tmmzuxt") should equal (5)
         new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstringOptimized("tmmzuxt") should equal (5)
         new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstringNaive("tmmzuxt") should equal (5)
     }
 
     "Longest Substring Without Repeating Characters of abba" should "2" in {
+        new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstringScala("abba") should equal (2)
         new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstringOptimized("abba") should equal (2)
         new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstringNaive("abba") should equal (2)
     }
 
     "Longest Substring Without Repeating Characters of ' '" should "1" in {
+        new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstringScala(" ") should equal (1)
         new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstringOptimized(" ") should equal (1)
         new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstringNaive(" ") should equal (1)
     }
 
     "Longest Substring Without Repeating Characters of abcabcbb" should "3" in {
+        new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstringScala("abcabcbb") should equal (3)
         new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstringOptimized("abcabcbb") should equal (3)
         new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstringNaive("abcabcbb") should equal (3)
     }
 
     "Longest Substring Without Repeating Characters of bbbbb" should "1" in {
+        new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstringScala("bbbbb") should equal (1)
         new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstringOptimized("bbbbb") should equal (1)
         new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstringNaive("bbbbb") should equal (1)
     }
 
     "Longest Substring Without Repeating Characters of pwwkew" should "3" in {
+        new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstringScala("pwwkew") should equal (3)
         new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstringOptimized("pwwkew") should equal (3)
         new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstringNaive("pwwkew") should equal (3)
     }
