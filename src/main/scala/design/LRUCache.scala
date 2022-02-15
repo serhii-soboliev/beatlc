62be325f1a8fc57ef88bcc876e3697150c596540
Serhii Soboliev <sergiy.sobolev@gmail.com>
2022-02-16 01:50:45 +0200

    [146. LRU Cache] ListBuffer solution

---
+++ src/main/scala/design/LRUCache.scala
@@ -0,0 +1,39 @@
+package org.sbk.leet
+package design
+
+/*
+146. LRU Cache
+https://leetcode.com/problems/lru-cache/
+ */
+class LRUCache(capacity: Int) {
+
+    import scala.collection.mutable.ListBuffer
+
+    private var cache: ListBuffer[(Int, Int)] = new ListBuffer()
+
+    def get(key: Int): Int = {
+        val i = find(key)
+        if(i != -1) {
+            val (k, v) = cache(i)
+            cache.remove(i)
+            cache = (k,v) +: cache
+            return v
+        }
+        -1
+    }
+
+    private def find(key: Int) : Int = {
+        cache.indexWhere(v => v._1 == key)
+    }
+
+    def put(key: Int, value: Int): Unit = {
+        val i = find(key)
+        if(i != -1) {
+            cache.remove(i)
+        } else if (cache.size == capacity) {
+            cache.remove(cache.size -1)
+        }
+        cache = (key,value) +: cache
+    }
+
+}
---
+++ src/test/scala/design/LRUCacheSpec.scala
@@ -0,0 +1,46 @@
+package org.sbk.leet
+package design
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+
+
+class LRUCacheSpec extends AnyFlatSpec with Matchers{
+
+    "LRUCacheSpec" should "scenario 1" in {
+        val lRUCache = new LRUCache(2)
+        lRUCache.put(1, 1)
+        lRUCache.put(2, 2)
+        lRUCache.get(1) should equal (1)
+        lRUCache.put(3, 3)
+        lRUCache.get(2) should equal (-1)
+        lRUCache.put(4, 4)
+        lRUCache.get(1) should equal (-1)
+        lRUCache.get(3) should equal (3)
+        lRUCache.get(4) should equal (4)
+    }
+
+    "LRUCacheSpec" should "scenario 2" in {
+        val lRUCache = new LRUCache(2)
+        lRUCache.get(2)
+        lRUCache.put(2, 6)
+        lRUCache.get(1) should equal (-1)
+        lRUCache.put(1, 5)
+        lRUCache.put(1, 2)
+        lRUCache.get(1) should equal (2)
+        lRUCache.get(2) should equal (6)
+    }
+
+    "LRUCacheSpec" should "scenario 3" in {
+        val lRUCache = new LRUCache(2)
+        lRUCache.put(2, 1)
+        lRUCache.put(1, 1)
+        lRUCache.put(2, 3)
+        lRUCache.put(4, 1)
+        lRUCache.get(1) should equal (-1)
+        lRUCache.get(2) should equal (3)
+
+    }
+
+}
