eb9b34533d586f95c6ec30684c2fc0eafab9b680
Serhii Soboliev <sergiy.sobolev@gmail.com>
2022-02-16 21:06:42 +0200

    [460. LFU Cache] Naive solution

---
+++ src/main/scala/design/LFUCacheNaive.scala
@@ -0,0 +1,54 @@
+package org.sbk.leet
+package design
+
+/*
+460. LFU Cache
+https://leetcode.com/problems/lfu-cache/
+ */
+class LFUCacheNaive(capacity: Int) {
+
+    import scala.collection.mutable
+
+    case class CacheValue(var value: Int, var usageCount: Int, var opNo: Int) extends Ordered[CacheValue] {
+        override def compare(that: CacheValue): Int =
+            this.usageCount - that.usageCount match {
+                case 0 => this.opNo.compare(that.opNo)
+                case other => other
+            }
+    }
+
+    private val cache = new mutable.HashMap[Int, CacheValue]()
+    private var opNo = 0
+
+    def get(key: Int): Int = {
+        if(capacity == 0) return -1
+        cache.get(key).map({ v =>
+            v.usageCount += 1
+            opNo += 1
+            v.opNo = opNo
+            return v.value
+        }).getOrElse(-1)
+    }
+
+    def put(key: Int, value: Int): Unit = {
+        if(capacity == 0) return
+        cache.get(key).map({ v =>
+            v.value = value
+            v.usageCount += 1
+            opNo += 1
+            v.opNo = opNo
+        }).getOrElse({
+            if(cache.size == capacity) evict()
+            opNo += 1
+            cache(key) = CacheValue(value, 1, opNo)
+        })
+
+    }
+
+    def evict(): Unit = {
+        if(cache.nonEmpty) cache.remove(cache.minBy(_._2)._1)
+    }
+
+}
+
+
---
+++ src/test/scala/design/LFUCacheSpec.scala
@@ -0,0 +1,52 @@
+package org.sbk.leet
+package design
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+
+class LFUCacheSpec extends AnyFlatSpec with Matchers{
+
+    "LFUCache" should "scenario 1" in {
+        val lfuCache = new LFUCacheNaive(2)
+        lfuCache.put(1,1)
+        lfuCache.put(2,2)
+        lfuCache.get(1) should equal(1)
+        lfuCache.put(3,3)
+        lfuCache.get(2) should equal(-1)
+        lfuCache.get(3) should equal(3)
+        lfuCache.put(4,4)
+        lfuCache.get(1) should equal(-1)
+        lfuCache.get(3) should equal(3)
+        lfuCache.get(4) should equal(4)
+    }
+
+    "LFUCache" should "scenario 2" in {
+        val lfuCache = new LFUCacheNaive(2)
+        lfuCache.put(3,1)
+        lfuCache.put(2,1)
+        lfuCache.put(2,2)
+        lfuCache.put(4,4)
+        lfuCache.get(2) should equal(2)
+    }
+
+    "LFUCache" should "scenario 3" in {
+        val lfuCache = new LFUCacheNaive(0)
+        lfuCache.put(0,0)
+        lfuCache.get(0) should equal(-1)
+    }
+
+    "LFUCache" should "scenario 4" in {
+        val lfuCache = new LFUCacheNaive(2)
+        lfuCache.put(2,1)
+        lfuCache.put(3,2)
+        lfuCache.get(3) should equal(2)
+        lfuCache.get(2) should equal(1)
+        lfuCache.put(4,3)
+        lfuCache.get(2) should equal(1)
+        lfuCache.get(3) should equal(-1)
+        lfuCache.get(4) should equal(3)
+    }
+
+
+}
