2ba0e690097cf12758bc3729eaadab6c45ca599e
Serhii Soboliev <sergiy.sobolev@gmail.com>
2022-02-17 02:03:35 +0200

    [1603. Design Parking System] Optimal refactored solution

--- src/main/scala/design/ParkingSystem.scala
+++ src/main/scala/design/ParkingSystem.scala
@@ -9,14 +9,21 @@ https://leetcode.com/problems/design-parking-system/
  */
 class ParkingSystem(_big: Int, _medium: Int, _small: Int) {
     import scala.collection.mutable
+
     private val slots = mutable.HashMap((1,_big),(2,_medium),(3, _small))
-    def addCar(carType: Int): Boolean = {
-        slots.get(carType).map(v => {
-            if(v == 0) return false
-            slots(carType) = v - 1
-            return true
-        })
-       false
+
+    def addCar(carType: Int): Boolean = slots.get(carType) match {
+        case None => false
+        case Some(value) if value == 0 =>
+            slots.remove(carType)
+            false
+        case Some(value) if value == 1 =>
+            slots.remove(carType)
+            true
+        case _ =>
+            slots(carType) -= 1
+            true
+
     }
 
 }
