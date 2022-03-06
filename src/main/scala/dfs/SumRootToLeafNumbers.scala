7047e43a451855c416f690232d2faefdca76e9f6
Serhii Soboliev <sergiy.sobolev@gmail.com>
2022-03-06 21:50:04 +0200

    [129. Sum Root to Leaf Numbers] DFC with reccursion

--- src/main/scala/dfs/SumRootToLeafNumbers.scala
+++ src/main/scala/dfs/SumRootToLeafNumbers.scala
@@ -1,13 +1,14 @@
 package org.sbk.leet
 package dfs
 
+
 /*
 129. Sum Root to Leaf Numbers
 https://leetcode.com/problems/sum-root-to-leaf-numbers/
  */
 class SumRootToLeafNumbers {
 
-    def sumNumbers(root: TreeNode): Int = {
+    def sumNumbersNaive(root: TreeNode): Int = {
         if(root == null) return 0
         def dfs(v: String, n: TreeNode): Int = {
             if (n.left == null && n.right == null) return (v + n.value).toInt
@@ -17,6 +18,21 @@ class SumRootToLeafNumbers {
             res
         }
         dfs("", root)
+    }
+
+    def sumNumbers(root: TreeNode): Int = {
+        import scala.util.control.TailCalls.{TailRec, done}
+        def dfs(base: Int, n: TreeNode): TailRec[Int] = n match {
+            case null => done(0)
+            case n if n.right == null && n.left == null => done(base*10 + n.value)
+            case _ =>
+                val newBase = base * 10 + n.value
+                for {
+                    left <- dfs(newBase, n.left)
+                    right <- dfs(newBase, n.right)
+                } yield left + right
+        }
+        dfs(0, root).result
     }
 }
 
