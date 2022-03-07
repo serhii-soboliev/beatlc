feb7c5bcef5313bf3fdefd3c870b91bb10bcbf02
Serhii Soboliev <sergiy.sobolev@gmail.com>
2022-03-08 00:04:02 +0200

    [99. Recover Binary Search Tree] In Order Traversal

---
+++ src/main/scala/dfs/RecoverBinarySearchTree.scala
@@ -0,0 +1,37 @@
+package org.sbk.leet
+package dfs
+
+
+
+/*
+99. Recover Binary Search Tree
+https://leetcode.com/problems/recover-binary-search-tree/
+ */
+class RecoverBinarySearchTree {
+
+    import utils.TreeNode
+
+    def recoverTree(n: TreeNode): Unit = {
+        var prevNode, fstToSwap, sndToSwap: TreeNode = null
+
+        def inorderTraverse(n: TreeNode): Unit = {
+            if (n != null) {
+                inorderTraverse(n.left)
+
+                if (prevNode != null && prevNode.value > n.value && fstToSwap == null) fstToSwap = prevNode
+                if (prevNode != null && prevNode.value > n.value && fstToSwap != null) sndToSwap = n
+
+                prevNode = n
+
+                inorderTraverse(n.right)
+            }
+        }
+
+        inorderTraverse(n)
+
+        val fstToSwapValue = fstToSwap.value
+        fstToSwap.value = sndToSwap.value
+        sndToSwap.value = fstToSwapValue
+    }
+
+}
---
+++ src/test/scala/dfs/RecoverBinarySearchTreeSpec.scala
@@ -0,0 +1,26 @@
+package org.sbk.leet
+package dfs
+
+import utils.TreeNode
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class RecoverBinarySearchTreeSpec extends AnyFlatSpec with Matchers{
+
+    "[1,3,null,null,2]" should "[3,1,null,null,2]" in {
+        val t = new RecoverBinarySearchTree()
+        val root = new TreeNode(1, new TreeNode(3, null, new TreeNode(2)))
+        t.recoverTree(root)
+        root.value should be (3)
+        root.left.value should be (1)
+    }
+
+    " [3,1,4,null,null,2]" should "[2,1,4,null,null,3]" in {
+        val t = new RecoverBinarySearchTree()
+        val root = new TreeNode(3, new TreeNode(1), new TreeNode(4, new TreeNode(2)))
+        t.recoverTree(root)
+        root.value should be (2)
+        root.right.left.value should be (3)
+    }
+
+}
