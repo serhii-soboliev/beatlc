a88ff60085bb7fe041f3ded10c5a506b159e0823
Serhii Soboliev <sergiy.sobolev@gmail.com>
2022-03-17 01:01:32 +0200

    [ 124. Binary Tree Maximum Path Sum ] Refactored solution

--- src/main/scala/dfs/BinaryTreeMaximumPathSum.scala
+++ src/main/scala/dfs/BinaryTreeMaximumPathSum.scala
@@ -13,10 +13,9 @@ class BinaryTreeMaximumPathSum {
         var res = Int.MinValue
         def dfs(tn: TreeNode): Int = {
             if(tn == null) return 0
-            val leftMax = dfs(tn.left) max 0
-            val rightMax = dfs(tn.right) max 0
-            res = res max (tn.value + leftMax + rightMax)
-            tn.value + (leftMax max rightMax)
+            val (l, r) = (dfs(tn.left), dfs(tn.right))
+            res = res max (tn.value + l + r)
+            (tn.value + (l max r)) max 0
         }
         dfs(root)
         res
