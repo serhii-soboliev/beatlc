fc0c13e204c3a5a83a9f05f8431ebd6cd278ddd5
Serhii Soboliev <sergiy.sobolev@gmail.com>
2022-03-19 01:20:04 +0200

    [ 1192. Critical Connections in a Network] Unit test

---
+++ src/main/scala/dfs/CriticalConnectionsInNetwork.scala
@@ -0,0 +1,14 @@
+package org.sbk.leet
+package dfs
+
+/*
+1192. Critical Connections in a Network
+https://leetcode.com/problems/critical-connections-in-a-network/
+ */
+class CriticalConnectionsInNetwork {
+
+    def criticalConnections(n: Int, connections: List[List[Int]]): List[List[Int]] = {
+        null
+    }
+
+}
---
+++ src/test/scala/dfs/CriticalConnectionsInNetworkSpec.scala
@@ -0,0 +1,14 @@
+package org.sbk.leet
+package dfs
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+
+class CriticalConnectionsInNetworkSpec extends AnyFlatSpec with Matchers {
+
+    "CriticalConnectionsInNetwork" should "scenario #1" in {
+        val ti = new CriticalConnectionsInNetwork()
+        ti.criticalConnections(4, List(List(0,1),List(1,2),List(2,0),List(1,3))) should be (List(List(0,1)))
+    }
+}
