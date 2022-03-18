21ca4e77e71051f57f73fd4d40ea6f57e8a16b79
Serhii Soboliev <sergiy.sobolev@gmail.com>
2022-03-18 22:00:45 +0200

    [ 687. Longest Univalue Path] fix tests

--- src/test/scala/dfs/BinaryTreeMaximumPathSumSpec.scala
+++ src/test/scala/dfs/BinaryTreeMaximumPathSumSpec.scala
@@ -20,7 +20,7 @@ class BinaryTreeMaximumPathSumSpec extends AnyFlatSpec with Matchers {
             new TreeNode(20, new TreeNode(15), new TreeNode(7)))) should be (42)
     }
 
-    "BinaryTreeMaximumPathSum [5,4,8,11,null,13,4, 7,2,null,null,null,1]" should "48" in {
+    "BinaryTreeMaximumPathSum [5,4,8,11,null,13,4, 7,2,null,null,null,1]" should "49" in {
         val ti = new BinaryTreeMaximumPathSum()
         ti.maxPathSumNaive(new TreeNode(5,
             new TreeNode(4,
@@ -28,7 +28,7 @@ class BinaryTreeMaximumPathSumSpec extends AnyFlatSpec with Matchers {
                 null),
             new TreeNode(8,
                 new TreeNode(13, null, new TreeNode(1)),
-                new TreeNode(4)))) should be (48)
+                new TreeNode(4)))) should be (49)
     }
 
     "BinaryTreeMaximumPathSumRec [9,-10,20,15,7]" should "42" in {
@@ -38,7 +38,7 @@ class BinaryTreeMaximumPathSumSpec extends AnyFlatSpec with Matchers {
             new TreeNode(20, new TreeNode(15), new TreeNode(7)))) should be (42)
     }
 
-    "BinaryTreeMaximumPathSumRec [5,4,8,11,null,13,4, 7,2,null,null,null,1]" should "48" in {
+    "BinaryTreeMaximumPathSumRec [5,4,8,11,null,13,4, 7,2,null,null,null,1]" should "49" in {
         val ti = new BinaryTreeMaximumPathSum()
         ti.maxPathSum(new TreeNode(5,
             new TreeNode(4,
@@ -46,7 +46,7 @@ class BinaryTreeMaximumPathSumSpec extends AnyFlatSpec with Matchers {
                 null),
             new TreeNode(8,
                 new TreeNode(13, null, new TreeNode(1)),
-                new TreeNode(4)))) should be (48)
+                new TreeNode(4)))) should be (49)
     }
 
 
