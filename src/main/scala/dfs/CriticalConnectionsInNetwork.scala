1b62696992367d47209aee138b9a503b66fc5bb9
Serhii Soboliev <sergiy.sobolev@gmail.com>
2022-03-20 02:18:39 +0200

    [1192. Critical Connections in a Network] Build graph method

--- src/main/scala/dfs/CriticalConnectionsInNetwork.scala
+++ src/main/scala/dfs/CriticalConnectionsInNetwork.scala
@@ -11,27 +11,34 @@ class CriticalConnectionsInNetwork {
         import scala.collection.mutable
         import scala.collection.mutable._
         var time = 0
-        val graph = new mutable.HashMap[Int, ListBuffer[Int]]()
+
         val result = new ListBuffer[List[Int]]()
         val visited = Array.fill[Boolean](n)(false)
         val timestamp = Array.fill[Int](n)(0)
+
+        def buildGraph(): mutable.HashMap[Int, ListBuffer[Int]] = {
+            val graph = new mutable.HashMap[Int, ListBuffer[Int]]()
+            connections foreach (l => {
+                graph(l.head) = graph.getOrElse(l.head, new ListBuffer[Int]()) += l.last
+                graph(l.last) = graph.getOrElse(l.last, new ListBuffer[Int]()) += l.head
+            })
+            graph
+        }
+
+        val g = buildGraph()
 
         def dfs(vertex: Int, prev: Int): Unit =  {
             visited(vertex) = true
             time += 1
             timestamp(vertex) = time
             val curTimeStamp = timestamp(vertex)
-            graph(vertex).filter(v => v != prev).foreach(v => {
+            g(vertex).filter(v => v != prev).foreach(v => {
                 if(!visited(v)) dfs(v, vertex)
                 timestamp(vertex) = timestamp(vertex) min timestamp(v)
                 if(curTimeStamp < timestamp(v)) result += List(vertex,v)
             })
         }
 
-        connections foreach (l => {
-                graph(l.head) = graph.getOrElse(l.head, new ListBuffer[Int]()) += l.last
-                graph(l.last) = graph.getOrElse(l.last, new ListBuffer[Int]()) += l.head
-        })
         dfs(0, -1)
         result.toList
     }
