e6daaf63a1e1cd3ebde7958f81d7d0c9b35fa7f6
Serhii Soboliev <sergiy.sobolev@gmail.com>
2022-03-28 22:16:21 +0300

    [ 2218. Maximum Value of K Coins From Piles] DP top-down approach

--- src/main/scala/contests/WC286.scala
+++ src/main/scala/contests/WC286.scala
@@ -57,6 +57,58 @@ class WC286 {
             case str => (str.dropRight (intLength & 1) ++ str.reverse).toLong
         })
     }
+
+    /*
+    2218. Maximum Value of K Coins From Piles
+    */
+    def maxValueOfCoins(piles: List[List[Int]], k: Int): Int = {
+        import scala.collection.mutable
+        import scala.collection.mutable._
+        val m = mutable.HashMap[(Int, Int), Int]()
+
+        def dp(piles: List[List[Int]], k: Int): Int = {
+            k match {
+                case 0 => 0
+                case _ => piles.indices.map(i => {
+                    if (piles(i).isEmpty) return 0
+                    val l = ListBuffer[List[Int]]()
+                    piles.indices.map(j => {
+                        if (i != j) l += piles(j)
+                        else if (piles(j).tail.nonEmpty) l += piles(j).tail
+                    })
+                    val t = m.getOrElseUpdate(Tuple2(l.toList.hashCode(), k-1), dp(l.toList, k - 1))
+                    piles(i).head + t
+                }).max
+            }
+        }
+
+        dp(piles,k)
+    }
+
+    def maxValueOfCoinsTopDown(piles: List[List[Int]], k: Int): Int = {
+        val n = piles.length
+        val memo = Array.fill[Array[Int]](n + 1)(Array.fill[Int](k + 1)(-1))
+
+        def dp(index: Int, p: List[List[Int]], k: Int): Int = {
+            if (k == 0 || index >= p.size) return 0
+            if (memo(index)(k) != -1) return memo(index)(k)
+            var m = 0
+            var sum = 0
+            m = m max dp(index + 1, p, k)
+            p(index).indices.foreach(i => {
+                sum += p(index)(i)
+                if (k - (i + 1) >= 0) {
+                    m = m max sum + dp(index + 1, p, k - (i + 1))
+                }
+            })
+            memo(index)(k) = m
+            m
+        }
+
+        dp(0, piles, k)
+    }
+
+
 }
 
 object WC286 {
@@ -66,6 +118,12 @@ object WC286 {
         println(o.kthPalindrome(
             Array(1,2,3,4,5,90), 5
         ).mkString("Array(", ", ", ")"))
+        println(o.maxValueOfCoinsTopDown(List(
+            List(37,88),
+            List(51,64,65,20,95,30,26),
+            List(9,62,20),
+            List(44)
+        ),9))
     }
 
 }
