305e674693875bba42ab0cc30f385f1f5ac360ca
Serhii Soboliev <sergiy.sobolev@gmail.com>
2022-04-01 01:42:01 +0300

    [1020. Number of Enclaves] Functional DFS scala solution

--- src/main/scala/dfs/CountSubIslands.scala
+++ src/main/scala/dfs/CountSubIslands.scala
@@ -7,7 +7,20 @@ https://leetcode.com/problems/count-sub-islands/
  */
 class CountSubIslands {
 
-    def countSubIslands(grid1: Array[Array[Int]], grid2: Array[Array[Int]]): Int = {
+    def countSubIslands(g1: Array[Array[Int]], g2: Array[Array[Int]]): Int = {
+
+        def f(i: Int, j: Int): Boolean = {
+            if (i < 0 || i >= g1.length || j < 0 || j >= g1(0).length || g2(i)(j) <= 0) return true
+            if (g2(i)(j) == 1 && g1(i)(j) != 1) return false
+            g2(i)(j) = -1
+            Seq((0, 1), (0, -1), (1, 0), (-1, 0)).map(d => f(i + d._1, j + d._2)).reduce(_ & _)
+        }
+
+        g1.indices.map(i => g1(0).indices.count(j => g2(i)(j) == 1 && f(i, j))).sum
+
+    }
+
+    def countSubIslandsNaive(grid1: Array[Array[Int]], grid2: Array[Array[Int]]): Int = {
 
         import scala.collection.mutable.ArrayBuffer
 
