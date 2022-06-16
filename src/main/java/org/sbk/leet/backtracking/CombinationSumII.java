4565bccf4654f57e939a6edff9448afdf2c2c753
Serhii Soboliev <sergiy.sobolev@gmail.com>
2022-06-16 22:34:36 +0300

    [40. Combination Sum II] backtracking solution with counter refactored

--- src/main/java/org/sbk/leet/backtracking/CombinationSumII.java
+++ src/main/java/org/sbk/leet/backtracking/CombinationSumII.java
@@ -3,6 +3,7 @@ package org.sbk.leet.backtracking;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /*
 40. Combination Sum II
@@ -10,10 +11,13 @@ https://leetcode.com/problems/combination-sum-ii/
  */
 public class CombinationSumII {
 
+    private TreeMap<Integer, Long> counter;
+    private List<Integer> counterKeys;
+    private final List<List<Integer>> results = new ArrayList<>();
+
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> results = new ArrayList<>();
         List<Integer> currentCombination = new ArrayList<>();
-        TreeMap<Integer, Long> counter = new TreeMap<> (Arrays
+        counter = new TreeMap<> (Arrays
                 .stream(candidates)
                 .boxed()
                 .collect(
@@ -22,23 +26,20 @@ public class CombinationSumII {
                                 Collectors.counting()
                         )
                 ));
-
-        backtrack(results, currentCombination, 0, counter, target);
+        counterKeys = new ArrayList<>(counter.keySet());
+        backtrack(currentCombination, 0, target);
         return results;
     }
 
-    private void backtrack(List<List<Integer>> results,
-                           List<Integer> currentCombination,
+    private void backtrack(List<Integer> currentCombination,
                            int currentKeyIndex,
-                           TreeMap<Integer, Long> counter,
                            int remain) {
         if(remain <= 0) {
-            if( remain == 0) {
+            if(remain == 0) {
                 results.add(currentCombination);
             }
             return;
         }
-        List<Integer> counterKeys = new ArrayList<>(counter.keySet());
         for(int i = currentKeyIndex; i<counterKeys.size(); i++) {
             Integer key = counterKeys.get(i);
             Long freq = counter.get(key);
@@ -48,7 +49,7 @@ public class CombinationSumII {
             counter.put(key, freq-1);
             List<Integer> newCombination = new ArrayList<>(currentCombination);
             newCombination.add(key);
-            backtrack(results, newCombination, i, counter, remain - key);
+            backtrack(newCombination, i, remain - key);
             counter.put(key, freq);
         }
 
