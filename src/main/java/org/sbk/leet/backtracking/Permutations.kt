0502c11241dac6611053640f965642745c26786a
Serhii Soboliev <sergiy.sobolev@gmail.com>
2022-06-17 21:31:40 +0300

    [46. Permutations] iterative backtracking solution

--- src/main/java/org/sbk/leet/backtracking/Permutations.kt
+++ src/main/java/org/sbk/leet/backtracking/Permutations.kt
@@ -1,5 +1,7 @@
 package org.sbk.leet.backtracking
 
+import org.scalactic.Bool
+
 /*
 46. Permutations
 https://leetcode.com/problems/permutations/
@@ -11,19 +13,51 @@ class Permutations {
             return arrayListOf()
         }
         if(nums.size == 1) {
-            return arrayListOf(nums.toList());
+            return arrayListOf(nums.toList())
+        }
+
+
+        fun dfs(nums: IntArray, result: MutableList<List<Int>>, current: MutableList<Int>, visited: BooleanArray) {
+            if(current.size == nums.size) {
+                result.add(current.toList())
+                return
+            }
+            for(i in nums.indices) {
+                if(visited[i]) {
+                    continue
+                }
+                visited[i] = true
+                current.add(nums[i])
+                dfs(nums, result, current, visited)
+                visited[i] = false
+                current.removeLast()
+            }
+        }
+
+        val result = mutableListOf<List<Int>>()
+        dfs(nums, result, mutableListOf(), BooleanArray(nums.size) { false })
+        return result
+    }
+
+
+    fun permuteRecursive(nums: IntArray): List<List<Int>> {
+        if(nums.isEmpty()) {
+            return arrayListOf()
+        }
+        if(nums.size == 1) {
+            return arrayListOf(nums.toList())
         }
 
         val result = mutableListOf<List<Int>>()
         val subNums = nums.copyOfRange(0, nums.lastIndex)
-        val subPermutations = permute(subNums);
+        val subPermutations = permuteRecursive(subNums)
         for(subPermutation in subPermutations) {
             subPermutation.forEachIndexed { i, _ ->
                 val newPermutation: MutableList<Int> = subPermutation.toMutableList()
                 newPermutation.add(i, nums.last())
                 result.add(newPermutation.toList())
             }
-            val lastPermutation = subPermutation.toMutableList();
+            val lastPermutation = subPermutation.toMutableList()
             lastPermutation.add(nums.last())
             result.add(lastPermutation)
         }
@@ -32,6 +66,6 @@ class Permutations {
     }
 }
 
-fun main(args: Array<String>) {
+fun main() {
     println(Permutations().permute(intArrayOf(1,2,3)))
 }
--- src/test/kotlin/org/sbk/leet/backtracking/PermutationsTest.kt
+++ src/test/kotlin/org/sbk/leet/backtracking/PermutationsTest.kt
@@ -11,8 +11,8 @@ import org.hamcrest.Matchers.hasSize
 internal class PermutationsTest {
 
     @Test
-    fun permute() {
-        val res = Permutations().permute(intArrayOf(1,2,3))
+    fun testPermuteRecursive1() {
+        val res = Permutations().permuteRecursive(intArrayOf(1,2,3))
         assertThat(res, hasSize(6))
         assertThat(res, containsInAnyOrder(arrayListOf(1,2,3),
                                            arrayListOf(1,3,2),
@@ -20,6 +20,19 @@ internal class PermutationsTest {
                                            arrayListOf(2,3,1),
                                            arrayListOf(3,1,2),
                                            arrayListOf(3,2,1)
+        ))
+    }
+
+    @Test
+    fun testPermuteIterative() {
+        val res = Permutations().permute(intArrayOf(1,2,3))
+        assertThat(res, hasSize(6))
+        assertThat(res, containsInAnyOrder(arrayListOf(1,2,3),
+                arrayListOf(1,3,2),
+                arrayListOf(2,1,3),
+                arrayListOf(2,3,1),
+                arrayListOf(3,1,2),
+                arrayListOf(3,2,1)
         ))
     }
 }
